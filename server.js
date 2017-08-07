a8478fd491239230e022e02c42c1f1f7171075bb
INDIRA10 <30663027+INDIRA10@users.noreply.github.com>
2017-08-07 16:08:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.get("/article-one",function(req, res){
 });
 
 app.get("/article-two",function(req, res){
- res.sendFile(path.join(_dirname, 'ui', 'article-two.htl')); 
+ res.sendFile(path.join(_dirname, 'ui', 'article-two.html')); 
 });
 
 app.get("/article-three",function(req, res){
-    res.send( "article three requested and will be served here");
+    res.sendFile(path.join(-dirname,'ui', 'article-three.html'))
     });
 
 app.get('/ui/style.css', function (req, res) {
