a746bbfcee9a2c2c13d9a3d2b163be7a46af4549
INDIRA10 <30663027+INDIRA10@users.noreply.github.com>
2017-08-13 21:54:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,5 @@ element.innerHTML = 'NEW VALUE-INDIRA RAJAGOPAL';
 //move the image
 var img=document.getElementByid('img');
 img.onclick=function () {
-img.style.marginleft= '100px';  
-    
+ img.style.marginleft= '100px';  
 };
