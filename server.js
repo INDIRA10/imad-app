1d2d5248bce31c7026f42ae9601f0c7aca6389c0
INDIRA10 <30663027+INDIRA10@users.noreply.github.com>
2017-08-24 13:49:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,12 +97,12 @@ app.get('/hash/:input',function (req, res) {
  });
  
  
-app.get('/article-db', function(req, res) {
+app.get('articles/:articleName', function(req, res) {
     //articleName == article-one
     //articles{articlename} == {} content object for article-one
     //SELECT*FROM article WHERE title = 'article-one'
    
-    pool.query("SELECT* FROM article where title= '" + req.params.articleName + "'",function(err,result){
+    pool.query("SELECT * FROM article where title= '" + req.params.articleName + "'",function(err,result){
        if (err){
        res.status(500).send(err.toString());
        } else {
