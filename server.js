31ca03edac36c27d7cc7f66469ceb4b12a7ea397
INDIRA10 <30663027+INDIRA10@users.noreply.github.com>
2017-08-24 23:05:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.get('/test-db', function(req, res){
 });
 
  
-app.get('articles/:articleName', function(req, res) {
+app.get('/articles/:articleName', function(req, res) {
     //articleName == article-one
     //articles{articleName} == {} content object for article-one
     //SELECT*FROM article WHERE title = 'article-one'
